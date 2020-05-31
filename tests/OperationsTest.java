dcce518c74fac553d71de736d35968212ecdcd1f
anis_huq <dodulhuq@gmail.com>
2020-05-31 03:01:54 -0700

    test class created

---
+++ tests/OperationsTest.java
@@ -0,0 +1,62 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OperationsTest {
+
+    @Test
+    public void sortDataSet() {
+    }
+
+    @Test
+    public void minimum() {
+    }
+
+    @Test
+    public void maximum() {
+    }
+
+    @Test
+    public void mode() {
+    }
+
+    @Test
+    public void median() {
+    }
+
+    @Test
+    public void arithmeticMean() {
+    }
+
+    @Test
+    public void meanAbsoluteDeviation() {
+    }
+
+    @Test
+    public void standardDeviation() {
+    }
+
+    @Test
+    public void loadFromDisplay() {
+    }
+
+    @Test
+    public void loadFromTxt() {
+    }
+
+    @Test
+    public void loadFromRandom() {
+    }
+
+    @Test
+    public void reset() {
+    }
+
+    @Test
+    public void sqrt() {
+    }
+
+    @Test
+    public void ceil() {
+    }
+}
